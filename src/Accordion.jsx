4c3bd25dce725d7bd9e619797baba5b0f3ee6ba4
Timineri Okoko <tonyeokoko@outlook.com>
2024-06-12 02:15:01 +0100

    Applied the useState on the accordion component

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
 export default function Accordion({ data }) {
   return (
     <div className="accordion">
-      {data.map(({ title, text }, i) => {
-        return (
-          <div className="item">
-            <p className="number">{i < 9 ? `0${i + 1}` : i + 1}</p>
-            <p className="title">{title}</p>
-            <p className="icon">-</p>
-            <div className="content-box">{text}</div>
-          </div>
-        );
+      {data.map((el, i) => {
+        return <AccordionItem title={el.title} text={el.text} num={i} />;
       })}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : " "}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
